74dce81a1bc7659b06485f1ba1f2fd33aa1c14a3
ysoroko <ysoroko@student.s19.be>
2021-09-28 16:14:39 +0200

    ex00 finished

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 14:08:17 by ysoroko           #+#    #+#             */
+/*   Updated: 2021/09/28 16:14:14 by ysoroko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+// Default constructor called
+// Copy constructor called
+// Assignation operator called // <-- Cette ligne est peut-etre absente
+// getRawBits member function called
+// Default constructor called
+// Assignation operator called
+// getRawBits member function called
+// getRawBits member function called
+// 0
+// getRawBits member function called
+// 0
+// getRawBits member function called
+// 0
+// Destructor called
+// Destructor called
+// Destructor called
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fixed_point_value);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	this->fixed_point_value = raw;
+}
+
+Fixed::Fixed()
+{
+	this->fixed_point_value = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed & Fixed::operator=( Fixed const & rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	setRawBits(rhs.getRawBits());
+	return (*this);
+}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,26 +6,43 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 14:08:20 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/09/28 11:52:16 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/09/28 15:52:58 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
+
+// CANONIC FORM
+// Class containing at least:
+// 1) Constructor default
+// 2) Copy constructor
+// 3) '=' Operator overload
+// 4) Destructor (virtual!)
+
 class	Fixed
 {
 	public:
-		int	fixed_point_value;
-		static const int n_fract_bits;
-
-	private:
 		Fixed();
 		~Fixed();
+
 		// A copy constructor
+		// A constructor which allows to create a copy of a different instance
+		Fixed(Fixed const & src);
+
+		// Operator '=' overload
+		// rhs = right hand side, a constant reference to our oject instance
+		// not a const function because our instance will be modified (ex: a = 14)
+		// Returns a reference to allow us to do "a = b = c = d"
 		Fixed & operator=( Fixed const & rhs);
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
 
+	private:
+		int	fixed_point_value;
+		static const int n_fract_bits = 8;
 };
+
 #endif
